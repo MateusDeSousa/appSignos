3aadb806581cc67529b2ec14eb999fb18dc599c2
Mateus de Sousa <mateusdevsousa@gmail.com>
2019-03-04 01:20:49 -0300

    added to the description of each sign in an alert

--- AppSignos/ViewController.swift
+++ AppSignos/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var signos: UITableView!
     let signo: [String] = ["Aries", "Touto", "Gemeos", "Cancer", "Leao", "Virgem", "Libra", "Escorpiao", "Sargitario", "Capricornio", "Aquario", "Peixes"]
+    let descriptionSignos: [String] = ["O ariano e uma pessoas cheia de energiae e entusiamo, lhe encantam as metas, a liberdade e as ideias novas.", "Zeloso e possessivo, um couro pode tender a ser inflexivel e ressentido. as vezes os touro pecam de ser cobicosos e e de permite-se tudo", "2 - ...", "3 - ...", "4 - ...", "5 - ...", "6 - ...", "7 - ...", "8 - ...", "9 - ...", "10 - ...", "11 - ..."]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +41,15 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate{
         cell.textLabel?.text = signo [indexPath.row]
         return cell
         
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let alert = UIAlertController(title: signo[indexPath.row], message: descriptionSignos[indexPath.row], preferredStyle: .alert)
+        let acaoConfirmar = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(acaoConfirmar)
+        present(alert, animated: true, completion: nil)
     }
     
 }
